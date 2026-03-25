0ae8ce560140bccb96c30314ccdcc51aad7c5496
G4brielXF <xavierdefreitasgabriel@gmail.com>
2026-03-25 09:05:45 -0300

    funcao renomear arquivo

--- sistema/Biblioteca/Upload.php
+++ sistema/Biblioteca/Upload.php
@@ -1,8 +1,9 @@
-<?php 
+<?php
 
 namespace sistema\Biblioteca;
 
-class Upload {
+class Upload
+{
 
     public $arquivo;
     public $subDiretorio;
@@ -13,32 +14,43 @@ class Upload {
     {
         $this->diretorio = $diretorio ?? 'uploads';
 
-        if(!file_exists($this->diretorio.DIRECTORY_SEPARATOR.$this->arquivo) && !is_dir($this->diretorio.DIRECTORY_SEPARATOR.$this->arquivo)){
+        if (!file_exists($this->diretorio . DIRECTORY_SEPARATOR . $this->arquivo) && !is_dir($this->diretorio . DIRECTORY_SEPARATOR . $this->arquivo)) {
             mkdir($this->diretorio, 0755);
         }
     }
 
-    public function arquivo(array $arquivo, string $subDiretorio = null): void
+    public function arquivo(array $arquivo, string $nome = null, string $subDiretorio = null): void
     {
         $this->arquivo = $arquivo;
+        $this->nome = $nome ?? pathinfo($arquivo['name'], PATHINFO_FILENAME);
         $this->subDiretorio = $subDiretorio ?? 'arquivos';
 
         $this->criarSubDiretorio();
+        $this->renomearArquivo();
         $this->moverArquivo();
     }
 
     public function criarSubDiretorio()
     {
-        if(!file_exists($this->diretorio.DIRECTORY_SEPARATOR.$this->subDiretorio) && !is_dir($this->diretorio.DIRECTORY_SEPARATOR.$this->subDiretorio)){
-            mkdir($this->diretorio.DIRECTORY_SEPARATOR.$this->subDiretorio, 0755);
+        if (!file_exists($this->diretorio . DIRECTORY_SEPARATOR . $this->subDiretorio) && !is_dir($this->diretorio . DIRECTORY_SEPARATOR . $this->subDiretorio)) {
+            mkdir($this->diretorio . DIRECTORY_SEPARATOR . $this->subDiretorio, 0755);
         }
+    }
+
+    public function renomearArquivo()
+    {
+        $arquivo = $this->nome . strrchr($this->arquivo['name'], '.');
+        if (file_exists($this->diretorio . DIRECTORY_SEPARATOR . $this->subDiretorio . DIRECTORY_SEPARATOR . $this->nome)) {
+            $arquivo = $this->nome . '-' . uniqid() . strrchr($this->arquivo['name'], '.');
+        }
+        $this->nome = $arquivo;
     }
 
     public function moverArquivo()
     {
-        if(move_uploaded_file($this->arquivo['tmp_name'], $this->diretorio.DIRECTORY_SEPARATOR.$this->subDiretorio.DIRECTORY_SEPARATOR.$this->arquivo['name'])){
-            echo $this->arquivo['name']. ' foi movido com sucesso!';
-        }else{
+        if (move_uploaded_file($this->arquivo['tmp_name'], $this->diretorio . DIRECTORY_SEPARATOR . $this->subDiretorio . DIRECTORY_SEPARATOR . $this->nome)) {
+            echo $this->nome . ' foi movido com sucesso!';
+        } else {
             echo 'Erro no upload!';
         }
     }
