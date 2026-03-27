0061256c5e9f73ca8001ebd754aa013f9312e4f6
G4brielXF <xavierdefreitasgabriel@gmail.com>
2026-03-27 11:24:06 -0300

    Fazendo Upload e armazenamento no DB

--- index.php
+++ index.php
@@ -3,25 +3,5 @@
 //Arquivo index responsável pela inicialização do sistema
 require 'vendor/autoload.php';
 
-//require 'rotas.php';
-use sistema\Biblioteca\Upload;
+require 'rotas.php';
 
-$upload = new Upload('upload');
-
-if(!empty($arquivo = $_FILES)){
-    $arquivo = $_FILES['arquivo'];
-    $upload->arquivo($arquivo, 'textos');
-}
-if($upload->getResultado()){
-    echo 'Arquivo enviado com sucesso!';
-}else{
-    $upload->getErro();
-}
-
-
-?>
-
-<form method="post" enctype="multipart/form-data">
-    <input type="file" name="arquivo"> 
-    <button>Enviar Arquivo</button>
-</form>
